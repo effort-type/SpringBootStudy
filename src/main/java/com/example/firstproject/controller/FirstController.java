9dca064553d693a3e84e8b2539f9711df7349c81
Minsu <jminsu084@gmail.com>
2022-07-25 19:43:54 +0900

    New : 뷰 템플릿과 MVC 패턴 적용

---
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -0,0 +1,15 @@
+package com.example.firstproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/hi") // hi라는 url입력이 들어오면 greetings 페이지를 반환해줌
+    public String niceToMeetYou(Model model){
+        model.addAttribute("username", "민수");
+        return "greetings"; // templates/greetings.mustache 파일을 찾아서 브라우저로 전송
+    }
+}
