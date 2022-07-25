eff45671937aa33ea8aa5c0fe2c1c257bbe65630
Minsu <jminsu084@gmail.com>
2022-07-25 19:51:02 +0900

    New : MVC를 적용한 seeYouNext 메소드 추가

--- src/main/java/com/example/firstproject/controller/FirstController.java
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -12,4 +12,10 @@ public class FirstController {
         model.addAttribute("username", "민수");
         return "greetings"; // templates/greetings.mustache 파일을 찾아서 브라우저로 전송
     }
+
+    @GetMapping("/bye")
+    public String seeYouNext(Model model){
+        model.addAttribute("nickname", "정민수");
+        return "goodbye";
+    }
 }
