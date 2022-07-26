47571f38820bd48dfc83301f95bd35077e489cb4
Minsu <jminsu084@gmail.com>
2022-07-26 17:24:28 +0900

    New : Form 태그를 통해 던진 데이터를 DTO 객체에 저장하기

---
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -0,0 +1,24 @@
+package com.example.firstproject.dto;
+
+/*
+* 데이터 던지는 갯수 만큼 변수를 선언해야함
+* */
+
+public class ArticleForm {
+
+    private String title;
+    private String content;
+
+    public ArticleForm(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "ArticleForm{" +
+                "title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
