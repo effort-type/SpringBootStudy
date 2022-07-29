ac951fa8c6f4df1dd6fc40c1e8f26d36d0b74132
Minsu <jminsu084@gmail.com>
2022-07-30 03:06:15 +0900

    Update : 롬복(Lombok)을 활용하여 DTO, Entity, Controller 리팩토링 완료

--- src/main/java/com/example/firstproject/dto/ArticleForm.java
+++ src/main/java/com/example/firstproject/dto/ArticleForm.java
@@ -1,28 +1,19 @@
 package com.example.firstproject.dto;
 
 /*
-* 데이터 던지는 갯수 만큼 변수를 선언해야함
-* */
+ * 데이터 던지는 갯수 만큼 변수를 선언해야함
+ * */
 
 import com.example.firstproject.entity.Article;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
+@AllArgsConstructor
+@ToString
 public class ArticleForm {
 
     private String title;
     private String content;
-
-    public ArticleForm(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "ArticleForm{" +
-                "title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 
     public Article toEntity() {
         return new Article(null, title, content);
--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -1,11 +1,18 @@
 package com.example.firstproject.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity //  DB가 해당 객체를 인식할 수 있도록 해주는 어노테이션
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Article {
 
     @Id // 대표값을 지정 ex) 주민등록번호
@@ -18,22 +25,4 @@ public class Article {
     @Column
     private String content;
 
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
-    public Article() {
-
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 }
