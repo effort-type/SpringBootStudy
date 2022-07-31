5b32fa68f1bd65e44b241a9da1f71e5eef3dd1a9
Minsu <jminsu084@gmail.com>
2022-07-31 17:22:36 +0900

    데이터 수정 폼 생성 및 링크 연결

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -73,4 +73,15 @@ public class ArticleController {
         return "articles/index";
     }
 
+    @GetMapping("/articles/{id}/edit")
+    public String edit(@PathVariable Long id, Model model) {
+        // 수정할 데이터를 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        // 모델에 데이터를 등록
+        model.addAttribute("article", articleEntity);
+        // 뷰 페이지 설정
+        return "/articles/edit";
+    }
+
 }
