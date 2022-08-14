c671ccaeca86870852f74427369581c256118488
Minsu <jminsu084@gmail.com>
2022-08-14 21:17:15 +0900

    Update : 댓글 목록을 출력하기 위해 Controller 수정

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,8 +1,10 @@
 package com.example.firstproject.controller;
 
 import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.dto.CommentDto;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import com.example.firstproject.service.CommentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
@@ -22,6 +24,9 @@ public class ArticleController {
 
     @Autowired // 스프링 부트가 미리 생성해둔 객체를 가져다가 자동 연결
     private ArticleRepository articleRepository;
+
+    @Autowired
+    private CommentService commentService;
 
     @GetMapping("/articles/new")
     public String newArticleForm() {
@@ -53,9 +58,12 @@ public class ArticleController {
         // 1. id로 데이터 가져오기
         // Optional<Article> articleEntity = articleRepository.findById(id); 모두가 java 8 버전이 아닐수도있기 때문에 지양
         Article articleEntity = articleRepository.findById(id).orElse(null); // .orElse(null)은 값이 없다면 null을 반환해라
+        List<CommentDto> commentDtos = commentService.comments(id);
 
         // 2. 가져온 데이터를 모델에 등록
         model.addAttribute("article", articleEntity);
+        model.addAttribute("commentDtos", commentDtos);
+
 
         // 3. 보여줄 페이지를 설정
         return "articles/show";
